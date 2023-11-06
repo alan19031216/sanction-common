ba9e46d7adab4e16794f1185eb67216ca56082b9
Alan <alan.tang1903@gmail.com>
2023-11-06 20:32:13 +0800

    Updates

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export * from "./middlewares/require-auth"
 
 export * from "./errors/bad-request-error"
 export * from "./errors/not-found-error"
+export * from "./errors/not-authorized-error"
 
 export * from "./services/jwt"
