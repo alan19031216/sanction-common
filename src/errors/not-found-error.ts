d4fd74fb6065e8aaea95997a98aa97b3d174fab1
Alan <alan.tang1903@gmail.com>
2023-11-06 20:25:52 +0800

    Updates

---
+++ src/errors/not-found-error.ts
@@ -0,0 +1,15 @@
+import { CustomError } from "./customer-error";
+
+export class NotFoundError extends CustomError {
+    statusCode = 404;
+
+    constructor() {
+        super("Not Found");
+
+        Object.setPrototypeOf(this, NotFoundError.prototype);
+    }
+
+    serializeError() {
+        return [{ message: "Not Found" }];
+    }
+}
